1122b53aac16a1f4b3679d634a232c85541ca632
j8761336 <105021026@live.asia.edu.tw>
2017-06-04 01:12:32 +0800

    0.0

---
+++ src/dandan/FrameMain.java
@@ -0,0 +1,9 @@
+package dandan;
+
+
+public class FrameMain {
+    public static void main(String args[]){
+        Ex2Frame ex2Frm = new Ex2Frame();
+        ex2Frm.setVisible(true);
+    }
+}
---
+++ src/dandan/LoginMain.java
@@ -0,0 +1,10 @@
+package dandan;
+
+public class LoginMain {
+
+	 public static void main(String args[]){
+	        Login login = new Login();
+	        login.setVisible(true);
+	    }
+
+}
